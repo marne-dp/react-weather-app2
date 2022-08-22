6d6073aa164eb73f902de43d75f0c9b085f50d09
marne-dp <104077297+marne-dp@users.noreply.github.com>
2022-08-22 10:39:24 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by
+        <a href="#">Marne du Preez</a> and is{" "}
+        <a
+          href="https://github.com/marne-dp/react-weather-app2"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
